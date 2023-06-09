af87393d28157a672dcae22841c192373eb1fb55
Timothy Salamatin <TJSalamatin@gmail.com>
2023-06-09 13:47:35 -0400

    Add files via upload

--- Javascript/script.js
+++ Javascript/script.js
@@ -7,7 +7,7 @@ var city
 var cityID
 var weekData = []
 
-//Cakll the city Data 
+//Call the city Data 
 function fetchCity(input) {
 
   fetch('https://api.openweathermap.org/data/2.5/weather?q=' + input + ',us&&appid=e498b6c6d940bacad67439963e87b103&units=imperial', {
@@ -18,9 +18,9 @@ function fetchCity(input) {
     })
     .then(function (data) {
       console.log(data)
-
+      // Convert the time into something readable, like the date
       var day = new Date(data.dt * 1000)
-
+      // Push data for todays data into the large display
       $("#day-0").find('#city-name').text(data.name)
       $("#day-0").find('.date').text(day.toDateString())
       $("#day-0").find('.icon').attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png")
@@ -28,6 +28,7 @@ function fetchCity(input) {
       $("#day-0").find('.windspeed').text('Wind Speed: ' + data.wind.speed + ' MPH')
       $("#day-0").find('.winddegree').text('@ ' + data.wind.deg + '°')
       $("#day-0").find('.humidity').text('Humidity Percentage: ' + data.main.humidity + ' %')
+      //Push day-0 data into the array to be stored
       weekData.push(data)
 
       fetchForecast(data)
@@ -52,12 +53,14 @@ function fetchForecast(citydata) {
     })
     .then(function (data) {
       console.log(data);
-
+      // Let it loop until we get the numeral we need to work with
       for (let index = 0; index < data.list.length; index++) {
+        // Grab the 5 days
         if (index == 0 || index == 8 || index == 16 || index == 24 || index == 32) {
+          // Add 1 to offset the days calculation to go from 1-5
           var dayIdent = (index / 8 + 1)
 
-          console.log('Temperature of Day ' + dayIdent + ' :' + data.list[index].main.temp);
+        
           // Pull and set data
           $("#day-" + dayIdent).find('.date').text(data.list[index].dt_txt.slice(0, 10))
           $("#day-" + dayIdent).find('.icon').attr("src", "http://openweathermap.org/img/w/" + data.list[index].weather[0].icon + ".png")
@@ -65,8 +68,8 @@ function fetchForecast(citydata) {
           $("#day-" + dayIdent).find('.windspeed').text('Wind: ' + data.list[index].wind.speed + ' MPH')
           $("#day-" + dayIdent).find('.winddegree').text('@ ' + data.list[index].wind.deg + '°')
           $("#day-" + dayIdent).find('.humidity').text('Humidity: ' + data.list[index].main.humidity + '%')
-
-
+          //Add it to the array
+          weekData.push(data)
 
 
 
@@ -93,7 +96,7 @@ function createButton(input) {
   btn.id = input;
   btn.classList.add("btn-class");
   btn.type = "button";
-
+// Append the button
   $('#button-container').append(btn)
 
 }
@@ -108,6 +111,7 @@ function storeCityData(keyname, data) {
 //Get City Data 
 function getCityData(keyname) {
   var oldData = JSON.parse(localStorage.getItem(keyname))
+  //Basically redo the previous code using the oldData array
   var day = new Date(oldData[0].dt * 1000)
   $("#day-0").find('#city-name').text(oldData[0].name)
   $("#day-0").find('.date').text(day.toDateString())
@@ -116,7 +120,7 @@ function getCityData(keyname) {
   $("#day-0").find('.windspeed').text('Wind: ' + oldData[0].wind.speed + ' MPH')
   $("#day-0").find('.winddegree').text('@ ' + oldData[0].wind.deg + '°')
   $("#day-0").find('.humidity').text('Humidity: ' + oldData[0].main.humidity + '%')
-
+  //Loop to add the days
   for (let index = 1; index < oldData.length; index++) {
     $("#day-" + index).find('.date').text(oldData[index].dt_txt)
     $("#day-" + index).find('.icon').attr("src", "http://openweathermap.org/img/w/" + oldData[index].weather[0].icon + ".png")
